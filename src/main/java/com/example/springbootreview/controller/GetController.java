8e1e8a58b260e5f5236a07f31f3f909522b8f0b1
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-10-28 00:40:23 +0900

    parameter get from url

--- src/main/java/com/example/springbootreview/controller/GetController.java
+++ src/main/java/com/example/springbootreview/controller/GetController.java
@@ -1,9 +1,6 @@
 package com.example.springbootreview.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class GetController {
@@ -23,6 +20,15 @@ public class GetController {
         return "Hello world";
     }
 
+    @GetMapping(value = "/variable1/{variable}")
+    public String getVariable1(@PathVariable String variable){
+        return variable;
+    }
+
+    @GetMapping(value = "/variable2/{variable}")
+    public String getVariable2(@PathVariable("variable") String var) {
+        return var;
+    }
 
 
 }
